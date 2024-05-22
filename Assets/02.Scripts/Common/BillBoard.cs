3a6a6d5d7f16dffd9ad29fc3e064c6a58b00019f
sjoonkim705 <sjoonkim705@gmail.com>
2024-05-22 16:03:33 +0900

    [김성준] 풀을 위한 커밋

--- Assets/02.Scripts/Common/BillBoard.cs
+++ Assets/02.Scripts/Common/BillBoard.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Billboard : MonoBehaviour
+public class BillBoard : MonoBehaviour
 {
     void Update()
     {
-        transform.rotation = Camera.main.transform.rotation;
+        transform.forward = Camera.main.transform.forward;
     }
 }
