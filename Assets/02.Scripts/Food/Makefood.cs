d9dbf91ff891c423de31487a09cf1f977918b5d6
PanDaJoa <jms3590743@gmail.com>
2024-05-23 17:21:12 +0900

    [전민성] 소환 놓기 오류수정

--- Assets/02.Scripts/Food/Makefood.cs
+++ Assets/02.Scripts/Food/Makefood.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,25 +10,28 @@ public class Makefood : MonoBehaviour
     public FoodType foodType;
     public Transform spawnPoint;  
     private bool isPlayerNearby = false; // 박스근처 확인
-
-    public void Awake()
-    {
-        
-    }
-    private void Start()
-    {
-
-    }
+ 
     void Update()
     {
+        
         if (isPlayerNearby && Input.GetKeyDown(KeyCode.E) && !Player.instance.IsHoldingFood())
         {
-            if (Player.instance != null)
-            {
-                Player.instance.SpawnFood(foodType, spawnPoint);
-            }
+            SpawnFood(foodType, Player.instance.handTransform);
         }
     }
+
+    public void SpawnFood(FoodType foodType, Transform spawnPoint)
+    {
+        // 음식 생성
+        Debug.Log("음식생성");
+        GameObject foodPrefab = FoodManager.instance.GetFoodPrefab(foodType);
+        if (foodPrefab != null)
+        {
+            GameObject food = Instantiate(foodPrefab, spawnPoint.position, spawnPoint.rotation);
+
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
