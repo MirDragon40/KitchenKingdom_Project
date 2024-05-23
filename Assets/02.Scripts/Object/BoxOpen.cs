db7620fd8805f146c4442fc00db3bb6973e9db47
yeeejini <rkdska231027@gmail.com>
2024-05-23 16:36:08 +0900

    [민예진] 박스 문열림

---
+++ Assets/02.Scripts/Object/BoxOpen.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxOpen : MonoBehaviour
+{
+    public Animator Animator;
+
+    private bool _isPlayerBox = false;
+
+    private void Update()
+    {
+        if (_isPlayerBox) 
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Animator.SetBool("PlayerBoxOpen", true);
+
+                StartCoroutine(BoxOpenAnimation());
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            _isPlayerBox = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) 
+        {
+            _isPlayerBox = false;
+        }
+    }
+    private IEnumerator BoxOpenAnimation() 
+    {
+        yield return new WaitForSeconds(1f);
+        Animator.SetBool("PlayerBoxOpen", false);
+    }
+}
