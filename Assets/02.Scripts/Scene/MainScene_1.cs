6b35858903cab32faa5a168ba60cb539b2ec0a9b
MirDragon40 <mirjeong6463@gmail.com>
2024-05-23 12:38:43 +0900

    [정수빈] 이것은 풀을 하기위한 커밋

--- Assets/02.Scripts/Scene/MainScene_1.cs
+++ Assets/02.Scripts/Scene/MainScene_1.cs
@@ -7,6 +7,8 @@ public class MainScene_1 : MonoBehaviourPunCallbacks
 {
    public static MainScene_1 Instance { get; private set; }
 
+    private bool _init = false;
+
     private void Awake()
     {
         if (Instance == null)
@@ -19,6 +21,26 @@ public class MainScene_1 : MonoBehaviourPunCallbacks
         }
     }
 
+    public void Start()
+    {
+        if (!_init)
+        {
+            Init();
+        }
+    }
 
+    public void Init()
+    {
+        _init = true;
+        PhotonNetwork.Instantiate($"Character", Vector3.zero, Quaternion.identity);
+
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            return;
+        }
+
+
+
+    }
 
 }
