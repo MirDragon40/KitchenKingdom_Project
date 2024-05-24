45a14ecc48733b9ba83e0a8de90fc21c2dce298a
PanDaJoa <jms3590743@gmail.com>
2024-05-24 11:31:05 +0900

    [전민성] 몬스터 에셋추가

--- Assets/02.Scripts/Monster/Bee.cs
+++ Assets/02.Scripts/Monster/Bee.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tresh : MonoBehaviour
+public class Bee : MonoBehaviour
 {
+    public float Health;
+    public float MaxHealth;
+    public float Speed;
+
     
     void Start()
     {
-        
+               
     }
 
+ 
     void Update()
     {
         
