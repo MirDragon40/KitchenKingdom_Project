84d940ad1f85a255b810cc558199dd7167a4efb2
MirDragon40 <mirjeong6463@gmail.com>
2024-05-28 10:04:54 +0900

    [정수빈] 커밋

--- Assets/02.Scripts/Object/Extinguisher.cs
+++ Assets/02.Scripts/Object/Extinguisher.cs
@@ -7,6 +7,8 @@ public class Extinguisher : IHoldable
     private ParticleSystem _powderEffect;
 
     public override Vector3 DropOffset => new Vector3(0f, 0f, 0f);
+
+    //public override Quaternion DropOffset_Rotation => Quaternion.Euler(0, -90, 0);
     private void Awake()
     {
         _powderEffect = GetComponentInChildren<ParticleSystem>();
@@ -18,8 +20,7 @@ public class Extinguisher : IHoldable
 
         // 각 아이템이 잡혔을 때 해줄 초기화 로직
         transform.parent = handTransform;
-
-         transform.localPosition = new Vector3(0.52f, 0f, -0.1f);
+         transform.localPosition = new Vector3(-0.042f, 0f, -0.068f);
         transform.localRotation = Quaternion.Euler(0, 90, 0);
 
     }
@@ -55,8 +56,9 @@ public class Extinguisher : IHoldable
 
         // 저장한 위치와 회전으로 소화기 배치
         transform.position = dropPosition;
-        transform.rotation = dropRotation;
-
+        //transform.rotation = dropRotation;
+        Quaternion additionalRotation = Quaternion.Euler(0, -90, 0);
+        Quaternion finalRotation = dropRotation * additionalRotation;
 
         transform.parent = null;
         // 각 아이템이 놓여질 때 해줄 초기화 로직
