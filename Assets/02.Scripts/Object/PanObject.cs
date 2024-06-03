fbb825cfbbe7f6969695c892ff0232ab404f64e2
PanDaJoa <jms3590743@gmail.com>
2024-06-03 11:10:18 +0900

    [전민성] 책상에서만 놓아지게 구현중

--- Assets/02.Scripts/Object/PanObject.cs
+++ Assets/02.Scripts/Object/PanObject.cs
@@ -4,8 +4,6 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
-
-
 public class PanObject : IHoldable
 {
     public Transform PanPlacePositon;
@@ -20,14 +18,15 @@ public class PanObject : IHoldable
     private Rigidbody _rigid;
     public override Vector3 DropOffset => new Vector3(0.3f, 0.1f, 0f);
 
+    private bool isOnSurface = false;  // 표면 위에 있는지 여부를 추적
+
     private void Awake()
     {
         BoxCollider = GetComponent<BoxCollider>();
-       
     }
+
     private void Update()
     {
-
         if (PanPlacePositon.childCount != 0)
         {
             PlusImage.SetActive(false);
@@ -43,18 +42,26 @@ public class PanObject : IHoldable
             }
             else
             {
-                GrillingIngrediant.StopGrilling();
+                if (GrillingIngrediant != null)
+                {
+                    GrillingIngrediant.StopGrilling();
+                }
             }
-
         }
         else
         {
+            if (GrillingIngrediant != null)
+            {
+                GrillingIngrediant.StopGrilling();
+            }
+
             GrillingIngrediant = null;
 
             GrillingSlider.gameObject.SetActive(false);
             PlusImage.SetActive(true);
         }
     }
+
     public override void Hold(Character character, Transform handTransform)
     {
         GetComponent<Rigidbody>().isKinematic = true;
@@ -63,60 +70,69 @@ public class PanObject : IHoldable
         transform.localRotation = Quaternion.Euler(-90f, 180f, 0f);
 
         MyStove = null;
+        isOnSurface = false;  // 아이템을 들 때는 표면에 있지 않음
     }
 
     public override void UnHold(Vector3 dropPosition, Quaternion dropRotation)
     {
-        GetComponent<Rigidbody>().isKinematic = false;
-        // 저장한 위치와 회전으로 음식 배치
-        transform.position = dropPosition;
-        //transform.rotation = dropRotation;
-        Quaternion pandropRotation = Quaternion.Euler(-90f, 180f, 0f);
-        Quaternion finalRotation = dropRotation * pandropRotation;
+        if (isOnSurface)  // 표면 위에 있을 때만 UnHold 동작
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            transform.position = dropPosition;
+            Quaternion pandropRotation = Quaternion.Euler(-90f, 180f, 0f);
+            Quaternion finalRotation = dropRotation * pandropRotation;
 
-
-        transform.parent = null;
-        //각 아이템이 떼어질 때 해줄 초기화 로직
-        _holdCharacter = null;
+            transform.parent = null;
+            _holdCharacter = null;
+        }
     }
 
     public override void Place(Transform place)
     {
         GetComponent<Rigidbody>().isKinematic = true;
         transform.position = place.position;
-            Quaternion panplaceRotation = Quaternion.Euler(-90, 0, 180);
-            transform.rotation = place.rotation * panplaceRotation;
-            transform.parent = place;
+        Quaternion panplaceRotation = Quaternion.Euler(-90, 0, 180);
+        transform.rotation = place.rotation * panplaceRotation;
+        transform.parent = place;
 
         MyStove = place.GetComponentInParent<Stove>();
-        //transform.rotation = placeRotation;
         _holdCharacter = null;
+        isOnSurface = true;  // 아이템을 놓을 때 표면 위에 있음
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!other.CompareTag("Player"))
+        if (other.CompareTag("Table"))  // 표면에 닿았을 때
         {
-            return;
+            isOnSurface = true;
         }
-        IHoldable playerHoldingItem = other.GetComponent<CharacterHoldAbility>().HoldableItem;
+        else if (other.CompareTag("Player"))
+        {
+            IHoldable playerHoldingItem = other.GetComponent<CharacterHoldAbility>().HoldableItem;
 
-        if (playerHoldingItem.GetComponent<FoodObject>().IsGrillable)
-        {
-            Debug.Log("grill");
-            other.GetComponent<CharacterHoldAbility>().PlacePosition = PanPlacePositon;
-            other.GetComponent<CharacterHoldAbility>().IsPlaceable = true;
+            if (playerHoldingItem != null && playerHoldingItem.GetComponent<FoodObject>().IsGrillable)
+            {
+                Debug.Log("grill");
+                other.GetComponent<CharacterHoldAbility>().PlacePosition = PanPlacePositon;
+                other.GetComponent<CharacterHoldAbility>().IsPlaceable = true;
+            }
         }
-        
     }
+
     private void OnTriggerExit(Collider other)
     {
-        other.GetComponent<CharacterHoldAbility>().PlacePosition = null;
-        other.GetComponent<CharacterHoldAbility>().IsPlaceable = false;
+        if (other.CompareTag("Table"))  // 표면에서 벗어났을 때
+        {
+            isOnSurface = false;
+        }
+        else
+        {
+            CharacterHoldAbility holdAbility = other.GetComponent<CharacterHoldAbility>();
+            if (holdAbility != null)
+            {
+                holdAbility.PlacePosition = null;
+                holdAbility.IsPlaceable = false;
+            }
+        }
     }
-
 }
-
-
-
-
