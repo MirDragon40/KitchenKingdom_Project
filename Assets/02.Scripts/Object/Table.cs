126c4678b80c8af2b07a64ebb50111bfbee1511a
PanDaJoa <jms3590743@gmail.com>
2024-06-07 13:04:17 +0900

    [전민성] 불 연쇄 적용 불 끄기 구현 중

--- Assets/02.Scripts/Object/Table.cs
+++ Assets/02.Scripts/Object/Table.cs
@@ -1,12 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class Table : MonoBehaviour
 {
     public ParticleSystem FireParticle;
+    public Table[] NearbyTables;
 
     public void Start()
     {
         FireParticle = GetComponentInChildren<ParticleSystem>();
+    }
+
+    public void Ignite()
+    {
+        FireParticle.Play();
+        StartCoroutine(SpreadFireToNearbyTables());
+    }
+
+    private IEnumerator SpreadFireToNearbyTables()
+    {
+        yield return new WaitForSeconds(5f);
+
+        foreach (var table in NearbyTables)
+        {
+            if (table != null)
+            {
+                table.Ignite();
+            }
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
