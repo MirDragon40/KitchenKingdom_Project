7900ffcaba0fd8f6e43ca9100d9efb309a83cb9f
yeeejini <rkdska231027@gmail.com>
2024-06-11 09:38:29 +0900

    [민예진] 출근ㅠ

--- Assets/02.Scripts/UI/UI_Option.cs
+++ Assets/02.Scripts/UI/UI_Option.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class UI_Option : MonoBehaviour
 {
-    
+    public void ClickResumeButton() 
+    {
+        GameManager.Instance.OptionUl.gameObject.SetActive(false);
+    }
+    public void ClickRestartButton() 
+    {
+        
+    }
+    public void ClickControlsButton() 
+    {
+        
+    }
+    public void ClickQuitButton() 
+    {
+        
+    }
+
 }
