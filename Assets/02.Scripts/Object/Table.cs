f76a07e5432b0604c6e80b7554b686b33b0b33c7
PanDaJoa <jms3590743@gmail.com>
2024-06-12 10:04:17 +0900

    [전민성] 출근 커밋

--- Assets/02.Scripts/Object/Table.cs
+++ Assets/02.Scripts/Object/Table.cs
@@ -3,26 +3,5 @@ using UnityEngine;
 
 public class Table : MonoBehaviour
 {
-    public Table[] NearbyTables;
-    public ParticleSystem FireParticle;
- 
-    public void Ignite()
-    {
-        StartCoroutine(SpreadFireToNearbyTables());
-    }
 
-    private IEnumerator SpreadFireToNearbyTables()
-    {
-        yield return new WaitForSeconds(5f);
-
-        foreach (var table in NearbyTables)
-        {
-            if (table != null)
-            {
-                Debug.Log("ㅇㅇㅇ");
-                table.FireParticle.Play();
-                table.Ignite();
-            }
-        }
-    }
 }
