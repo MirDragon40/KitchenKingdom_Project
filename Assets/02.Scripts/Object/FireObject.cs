d1555aacd0467cb031e694e29dbc2adad1911aed
PanDaJoa <jms3590743@gmail.com>
2024-06-14 11:54:49 +0900

    [전민성] 불 완료

--- Assets/02.Scripts/Object/FireObject.cs
+++ Assets/02.Scripts/Object/FireObject.cs
@@ -1,48 +1,49 @@
-using Photon.Pun;
 using System.Collections;
 using UnityEngine;
 
 public class FireObject : MonoBehaviour
 {
-    public PanObject panObject;
+    public bool _isOnFire = false;
+    public ParticleSystem fireEffect;
+    private bool isPowderTouching = false;
+    public float contactTime = 0;
 
-    public ParticleSystem FireEffect;
-    public bool _isOnFire = false;   // 불이 붙어있는 상태인지 나타냄
-    public float contactTime = 0f;   // 'Powder'와의 접촉 시간을 측정
-    public PhotonView PhotonView;
-    private void Awake()
+    private void Start()
     {
-       FireEffect = GetComponentInChildren<ParticleSystem>();
-        PhotonView = GetComponent<PhotonView>();
+        fireEffect = GetComponentInChildren<ParticleSystem>();
     }
 
     public void MakeFire()
     {
-        if (_isOnFire)
-        {
-            return;
-        }
-        PhotonView.RPC("RPC_MakeFire", RpcTarget.All);
+        _isOnFire = true;
+        fireEffect.Play();
     }
 
     public void Extinguish()
     {
-        PhotonView.RPC("RPC_Extinguish", RpcTarget.All);
-    }
-    [PunRPC]
-    public void RPC_MakeFire()
-    {
-        FireEffect.Play();
-        _isOnFire = true;
-        Debug.Log("불이야");
-    }
-    
-    [PunRPC]
-    public void RPC_Extinguish()
-    {
-        FireEffect.Stop();
-        contactTime = 0f;
         _isOnFire = false;
-        Debug.Log("불꺼짐");
+        fireEffect.Stop();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (_isOnFire && other.CompareTag("Powder"))
+        {
+            isPowderTouching = true;
+            contactTime += Time.deltaTime;
+            if (contactTime >= 2f)
+            {
+                Extinguish();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (_isOnFire && other.CompareTag("Powder"))
+        {
+            isPowderTouching = false;
+            contactTime = 0f;
+        }
     }
 }
