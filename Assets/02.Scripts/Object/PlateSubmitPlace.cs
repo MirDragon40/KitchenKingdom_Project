ec5ef0e1c2ad9afad1fa2aab14a966a240e00ffb
PanDaJoa <jms3590743@gmail.com>
2024-06-20 12:55:41 +0900

    [전민성] 콜라사운드 캐릭터가 음식을 만들고 제출시 손이 내려가지 않는거 수정

--- Assets/02.Scripts/Object/PlateSubmitPlace.cs
+++ Assets/02.Scripts/Object/PlateSubmitPlace.cs
@@ -16,7 +16,6 @@ public class PlateSubmitPlace : MonoBehaviour
     private string _plateContent = string.Empty;
     public TMP_Text ScoreUI;
     private PhotonView _pv;
-
     private void Awake()
     {
         _pv = GetComponent<PhotonView>();
@@ -60,7 +59,8 @@ public class PlateSubmitPlace : MonoBehaviour
         }
         _foodCombo = null;
         _plateContent = string.Empty;
-        
+
+        _holdability._pv.RPC("Drop", RpcTarget.All);
     }
     // todo : 손에 들고있는 plate에 맞는 음식을 제출했을때 ordermanager의 내용과 비교하여 gamemanager의 totalscore 25점 더하기
     // 
