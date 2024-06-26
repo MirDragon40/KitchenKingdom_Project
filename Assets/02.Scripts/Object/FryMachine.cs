7e0938746340f0d6efb3003ad81caaefc48b7d32
PanDaJoa <jms3590743@gmail.com>
2024-06-26 15:51:35 +0900

    [전민성] 튀김기 불 소리 등등 구현 불이 꺼지면 사운드도 꺼지게 구현 중

--- Assets/02.Scripts/Object/FryMachine.cs
+++ Assets/02.Scripts/Object/FryMachine.cs
@@ -6,9 +6,18 @@ public class FryMachine : CookStand
 {
     public BasketObject PlacedBasket = null;
     private Transform _originalPlacePosition;
-
+    public FireObject fireObject;
+    public SoundManager SoundManager;
     public bool IsBasketPlaced => PlacedBasket != null;
 
+    private bool isFireExtinguished = true;
+    private Coroutine igniteCoroutine;
+    public Table[] NearbyTables;
+    private void Awake()
+    {
+        fireObject = GetComponent<FireObject>();
+        SoundManager = FindObjectOfType<SoundManager>();
+    }
     private void Start()
     {
         _originalPlacePosition = base.PlacePosition;
@@ -28,6 +37,69 @@ public class FryMachine : CookStand
             PlacedBasket = null;
         }
 
+        if (IsBasketPlaced && PlacedBasket.MyFryMachine == this)
+        {
+            if (PlacedBasket != null)
+            {
+
+                if (PlacedBasket.fireObject._isOnFire)
+                {
+                    if (!fireObject._isOnFire)
+                    {
+                        fireObject.RequestMakeFire();
+                        SoundManager.PlayAudio("FireSound", true);
+                    }
+                    else if (isFireExtinguished)
+                    {
+                        igniteCoroutine = StartCoroutine(IgniteNearbyTables());
+                    }
+                }
+                else
+                {
+                    if (igniteCoroutine != null)
+                    {
+                        StopCoroutine(igniteCoroutine);
+                        igniteCoroutine = null;
+                        isFireExtinguished = true;
+                    }
+                }
+            }
+            else
+            {
+
+                if (fireObject._isOnFire)
+                {
+                    fireObject.RequestExtinguish();
+                    isFireExtinguished = true;
+                    if (igniteCoroutine != null)
+                    {
+                        StopCoroutine(igniteCoroutine);
+                        igniteCoroutine = null;
+                    }
+                }
+            }
+
+
+        }
+
     }
 
+    public IEnumerator IgniteNearbyTables()
+    {
+        isFireExtinguished = false;
+        yield return new WaitForSeconds(5f);
+
+        foreach (var table in NearbyTables)
+        {
+            if (!table.IsOnFire)
+            {
+                table.Ignite();
+            }
+        }
+        // 코루틴이 끝나면 초기화
+        igniteCoroutine = null;
+        isFireExtinguished = true;
+    }
 }
+
+
