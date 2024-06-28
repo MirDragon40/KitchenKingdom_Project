88300bc49bd90b3acfdafbea04beeaff53c3d1a4
MirDragon40 <mirjeong6463@gmail.com>
2024-06-28 15:28:21 +0900

    [정수빈] 실시간 점수 업데이트

--- Assets/02.Scripts/UI/UI_ScoreStar.cs
+++ Assets/02.Scripts/UI/UI_ScoreStar.cs
@@ -13,6 +13,21 @@ public class UI_ScoreStar : MonoBehaviour
     }
     void Update()
     {
-        ScoreText.text = $"{GameManager.Instance.TotalScore} / 100";
+        if(GameManager.Instance.CurrentStage == 1)
+        {
+            ScoreText.text = $"{GameManager.Instance.StageScore[0]} / 100";
+        }
+        else if (GameManager.Instance.CurrentStage == 2)
+        {
+            ScoreText.text = $"{GameManager.Instance.StageScore[1]} / 100";
+        }
+        else if (GameManager.Instance.CurrentStage == 3)
+        {
+            ScoreText.text = $"{GameManager.Instance.StageScore[2]} / 100";
+        }
+        else if (GameManager.Instance.CurrentStage == 4)
+        {
+            ScoreText.text = $"{GameManager.Instance.StageScore[3]} / 100";
+        }
     }
 }
